c21bdbef0c50d9e7ef2bb86a0670722afef347a3
Aymen <aymenghaloua@gmail.com>
2024-06-21 14:07:43 +0100

    feat: -Implementing "Mongoose" and MongoDB; -Adding "Image, Transaction and User" Model.

---
+++ lib/database/mongoose.ts
@@ -0,0 +1,30 @@
+import mongoose, { Mongoose } from 'mongoose';
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+interface MongooseConnection {
+  connection: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+let cached: MongooseConnection = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = {
+    connection: null,
+    promise: null,
+  };
+}
+
+export const connectedToDatabase = async () => {
+  if (cached.connection) return cached.connection;
+
+  if (!MONGODB_URL) {
+    throw new Error('Please define the MONGODB_URL environment variable inside .env.local');
+  }
+
+  if (cached.promise) return cached.promise || mongoose.connect(MONGODB_URL, { dbName: 'PhotoTask', bufferCommands: false });
+
+  cached.connection = await cached.promise;
+  return cached.connection;
+};
