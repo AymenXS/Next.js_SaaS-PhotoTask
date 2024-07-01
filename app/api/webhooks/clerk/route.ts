160a9f4fd8650470d649a984e22b3efb3f5c9ede
Aymen <aymenghaloua@gmail.com>
2024-07-01 20:25:33 +0100

    fix: Fixing TypeScript Errors.

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -34,11 +34,11 @@ export async function POST(req: Request) {
   // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
 
-  let evt: WebhookEvent;
+  let event: WebhookEvent;
 
   // Verify the payload with the headers
   try {
-    evt = wh.verify(body, {
+    event = wh.verify(body, {
       'svix-id': svix_id,
       'svix-timestamp': svix_timestamp,
       'svix-signature': svix_signature,
@@ -51,19 +51,19 @@ export async function POST(req: Request) {
   }
 
   // Get the ID and type
-  const { id } = evt.data;
-  const eventType = evt.type;
+  const { id } = event.data;
+  const eventType = event.type;
 
   // CREATE
   if (eventType === 'user.created') {
-    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+    const { id, email_addresses, image_url, first_name, last_name, username } = event.data;
 
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name!,
+      lastName: last_name!,
       photo: image_url,
     };
 
@@ -83,11 +83,11 @@ export async function POST(req: Request) {
 
   // UPDATE
   if (eventType === 'user.updated') {
-    const { id, image_url, first_name, last_name, username } = evt.data;
+    const { id, image_url, first_name, last_name, username } = event.data;
 
     const user = {
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name!,
+      lastName: last_name!,
       username: username!,
       photo: image_url,
     };
@@ -99,7 +99,7 @@ export async function POST(req: Request) {
 
   // DELETE
   if (eventType === 'user.deleted') {
-    const { id } = evt.data;
+    const { id } = event.data;
 
     const deletedUser = await deleteUser(id!);
 
