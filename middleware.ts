28c25b8d968f3531a74589d27542a195ce1564ea
Aymen <aymenghaloua@gmail.com>
2024-07-01 20:18:29 +0100

    fix: Fixing Middleware and Clerk's Webhooks Route

--- middleware.ts
+++ middleware.ts
@@ -12,21 +12,3 @@ export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
 
-// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-
-// // Define the public routes
-// const publicRoutes = createRouteMatcher(['/', '/api/webhooks/clerk', '/api/webhooks/stripe', '/sign-in(.*)', '/sign-up(.*)']);
-
-// export default clerkMiddleware((auth, request) => {
-//   // If the route is public, allow access without authentication
-//   if (publicRoutes(request)) {
-//     return;
-//   }
-
-//   // Otherwise, protect the route
-//   auth().protect();
-// });
-
-// export const config = {
-//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-// };
