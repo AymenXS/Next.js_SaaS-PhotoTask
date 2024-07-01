3350608826bd12c7033f3409c467113d8933873f
Aymen <aymenghaloua@gmail.com>
2024-07-01 20:13:27 +0100

    Updating Middleware.

--- middleware.ts
+++ middleware.ts
@@ -1,32 +1,32 @@
-// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-
-// const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
-
-// export default clerkMiddleware((auth, request) => {
-//   if (!isPublicRoute(request)) {
-//     auth().protect();
-//   }
-// });
-
-// export const config = {
-//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-// };
-
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-// Define the public routes
-const publicRoutes = createRouteMatcher(['/', '/api/webhooks/clerk', '/api/webhooks/stripe']);
+const isPublicRoute = createRouteMatcher(['/', '/api/webhooks/clerk', '/api/webhooks/stripe', '/sign-in(.*)', '/sign-up(.*)']);
 
 export default clerkMiddleware((auth, request) => {
-  // If the route is public, allow access without authentication
-  if (publicRoutes(request)) {
-    return;
+  if (!isPublicRoute(request)) {
+    auth().protect();
   }
-
-  // Otherwise, protect the route
-  auth().protect();
 });
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
+// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+// // Define the public routes
+// const publicRoutes = createRouteMatcher(['/', '/api/webhooks/clerk', '/api/webhooks/stripe', '/sign-in(.*)', '/sign-up(.*)']);
+
+// export default clerkMiddleware((auth, request) => {
+//   // If the route is public, allow access without authentication
+//   if (publicRoutes(request)) {
+//     return;
+//   }
+
+//   // Otherwise, protect the route
+//   auth().protect();
+// });
+
+// export const config = {
+//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
